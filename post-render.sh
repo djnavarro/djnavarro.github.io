b2120bb994f1e3bb9c550f88aa177f33951e683c
Danielle Navarro <djnavarro@protonmail.com>
2024-10-25 16:45:41 +1100

    adds .well-known

--- post-render.sh
+++ post-render.sh
@@ -1,3 +1,5 @@
 touch docs/.nojekyll
 echo 'djnavarro.net' > docs/CNAME
 cp image/danielle-3.jpg docs/image/danielle-3.jpg
+mkdir docs/.well-known
+cp .well-known/atproto-did docs/.well-known/atproto-did
